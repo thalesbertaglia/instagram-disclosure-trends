8cc532b967c87b999cec17d0e1e24e371252d292
thalesbertaglia <contact@thalesbertaglia.com>
2023-09-19 11:21:59 +0200

    Add generate latex table

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 from typing import Dict, Any
 
+import pyperclip
+
 
 def safe_get(dictionary: Dict[str, Any], *keys, default=None) -> Any:
     """
@@ -11,3 +13,27 @@ def safe_get(dictionary: Dict[str, Any], *keys, default=None) -> Any:
         except KeyError:
             return default
     return dictionary or default
+
+
+def generate_latex_table(
+    df_table: "pd.DataFrame",
+    table_caption: str,
+    table_label: str,
+    columns_rename_map: Dict[str, str] = {},
+    index_rename_map: Dict[str, str] = {},
+):
+    n_columns = len(df_table.columns)
+    pyperclip.copy(
+        df_table.rename(columns=columns_rename_map, index=index_rename_map)
+        .style.format(precision=2)
+        .applymap_index(lambda v: "font-weight: bold;", axis="columns")
+        .applymap_index(lambda v: "font-weight: bold;", axis="index")
+        .to_latex(
+            hrules=True,
+            convert_css=True,
+            column_format=f"l{'c'*n_columns}",
+            caption=table_caption,
+            label=table_label,
+            position_float="centering",
+        )
+    )
